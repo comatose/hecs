28281a37cd88cba7039e7c35755d8d2adb7a6040
comatose <comatose@ubuntu.(none)>
2013-03-03 20:25:55 +0900

    * src/HECS.hs:

--- src/HECS.hs
+++ src/HECS.hs
@@ -7,11 +7,11 @@ module Main where
 import           Codec.Encryption.Padding (pkcs5, unPkcs5)
 import qualified Codec.FEC                as F
 import           Codec.Utils              (listFromOctets, listToOctets)
-import           Control.Exception        (handle)
+-- import           Control.Exception        (handle)
 import           Control.Monad
-import           Data.Array
 import qualified Data.ByteString          as B
 import qualified Data.ByteString.Internal as B
+import qualified Data.Vector              as V
 import           Foreign.C.Error
 import           Foreign.ForeignPtr       (withForeignPtr)
 import           Foreign.Ptr              (plusPtr)
@@ -23,14 +23,16 @@ import           System.IO
 import           System.Posix
 import qualified Text.JSON.Generic        as J
 
-defaultChunkSize :: ByteCount
+defaultChunkSize :: (Integral a) => a
 defaultChunkSize = 4
 
 data Config = Config{ primaryNodes :: [FilePath], secondaryNodes :: [FilePath] } deriving (J.Typeable, J.Data, Show)
 
-data HECS = HECS (Array Int Fd) (Array Int Fd)
+data HECS = HECS (V.Vector Fd) (V.Vector Fd)
 
 type ChunkIndex = Int
+
+type StripeIndex = Int
 
 asPrimary :: String -> Config -> [String]
 asPrimary path cfg = map (++ path) (primaryNodes cfg)
@@ -54,22 +56,12 @@ main = do
   fuseMain (hecsFSOps cfg) hecsExceptionHandler
 
 hecsExceptionHandler :: SomeException -> IO Errno
-hecsExceptionHandler _  = getErrno
--- hecsExceptionHandler ioe
---     | isAlreadyExistsError ioe = return eALREADY
---     | isDoesNotExistError  ioe = return eNOENT
---     | isAlreadyInUseError  ioe = return eBUSY
---     | isFullError          ioe = return eAGAIN
---     | isEOFError           ioe = return eIO
---     | isIllegalOperation   ioe = return eNOTTY
---     | isPermissionError    ioe = return ePERM
---     | otherwise                = return eFAULT
+hecsExceptionHandler _  = getErrno >>= (\(Errno errno) -> return . Errno . negate $ errno)
 
 hecsFSOps :: Config -> FuseOperations HT
 hecsFSOps cfg =
     defaultFuseOps {
       fuseGetFileStat = hecsGetFileStat cfg
-      , fuseGetFileSystemStats = hecsGetFileSystemStats
 
       , fuseCreateDirectory = hecsCreateDirectory cfg
       , fuseOpenDirectory = hecsOpenDirectory cfg
@@ -79,6 +71,7 @@ hecsFSOps cfg =
       , fuseRename = hecsRename cfg
       , fuseSetFileMode = hecsSetFileMode cfg
       , fuseSetFileTimes = hecsSetFileTimes cfg
+      , fuseSetFileSize = hecsSetFileSize cfg
 
       , fuseCreateDevice = hecsCreateDevice cfg
       , fuseOpen = hecsOpen cfg
@@ -94,7 +87,7 @@ hecsFSOps cfg =
 
       -- , fuseCreateSymbolicLink = hecsCreateSymbolicLink cfg
       -- , fuseReadSymbolicLink = hecsReadSymbolicLink cfg
-      -- , fuseSetFileSize = hecsSetFileSize cfg
+      -- , fuseGetFileSystemStats = hecsGetFileSystemStats
       }
 
 fileStatusToEntryType :: FileStatus -> EntryType
@@ -126,19 +119,15 @@ fileStatusToFileStat status size' =
 
 hecsGetFileStat :: Config -> FilePath -> IO (Either Errno FileStat)
 hecsGetFileStat cfg path =
-  handle (\(_ :: SomeException) -> fmap Left getErrno) $ do
-    let paths = asPrimary path cfg
-    status <- getSymbolicLinkStatus . head $ paths
-    size' <- fmap (sum . map fileSize) $ mapM getSymbolicLinkStatus (tail paths)
-    return $ Right $ fileStatusToFileStat status size'
-
-hecsGetFileSystemStats :: String -> IO (Either Errno FileSystemStats)
-hecsGetFileSystemStats _ = return (Left eOK)
+  -- handle (\(_ :: SomeException) -> fmap Left getErrno) $
+  do let paths = asPrimary path cfg
+     status <- getSymbolicLinkStatus . head $ paths
+     size' <- fmap (sum . map fileSize) $ mapM getSymbolicLinkStatus (tail paths)
+     return $ Right $ fileStatusToFileStat status size'
 
 hecsCreateDirectory :: Config -> FilePath -> FileMode -> IO Errno
 hecsCreateDirectory cfg path mode =
     do let paths = asPrimary path cfg ++ asSecondary path cfg
-       print paths
        mapM_ (`createDirectory` mode) paths
        return eOK
 
@@ -149,14 +138,14 @@ hecsOpenDirectory cfg path =
 
 hecsReadDirectory :: Config -> FilePath -> IO (Either Errno [(FilePath, FileStat)])
 hecsReadDirectory cfg path0 =
-  let path = head (asPrimary path0 cfg)
-      pairType :: FilePath -> IO (FilePath, FileStat)
+  let pairType :: FilePath -> IO (FilePath, FileStat)
       pairType name = hecsGetFileStat cfg (path0 ++ "/" ++ name)
                       >>= either
                       (\errno -> throwIO $ errnoToIOError "hecsReadDirectory" errno Nothing (Just name))
                       (\stat -> return (name, stat))
-  in handle (\(_ :: SomeException) -> fmap Left getErrno) $
-    fmap Right (getDirectoryContents path >>= mapM pairType)
+  in
+   -- handle (\(_ :: SomeException) -> fmap Left getErrno) $
+   fmap Right $ (getDirectoryContents . head . asPrimary path0 $ cfg) >>= mapM pairType
 
 hecsRemoveDirectory :: Config -> FilePath -> IO Errno
 hecsRemoveDirectory cfg path =
@@ -192,14 +181,15 @@ hecsCreateDevice cfg path0 entryType mode dev =
 
 hecsOpen :: Config -> FilePath -> OpenMode -> OpenFileFlags -> IO (Either Errno HT)
 hecsOpen cfg path0 mode flags =
-    do ps <- mapM (\path -> openFd path mode Nothing flags) (asPrimary path0 cfg)
-       ss <- mapM (\path -> openFd path mode Nothing flags) (asSecondary path0 cfg)
-       return . Right $ HECS (listArray (0, length ps - 1) ps) (listArray (0, length ss - 1) ss)
+  -- handle (\(_ :: SomeException) -> fmap Left getErrno) $
+  do ps <- mapM (\path -> openFd path mode Nothing flags) (asPrimary path0 cfg)
+     ss <- mapM (\path -> openFd path mode Nothing flags) (asSecondary path0 cfg)
+     return . Right $ HECS (V.fromList ps) (V.fromList ss)
 
 quotRem' :: (Integral a, Integral a1, Num t, Num t1) => a -> a1 -> (t, t1)
 quotRem' x y = let (q, r) = quotRem (fromIntegral x) (fromIntegral y) in (fromIntegral q, fromIntegral r)
 
-toPhyAddrs :: Array Int Fd -> FileOffset -> ByteCount -> [(Fd, FileOffset, ByteCount)]
+toPhyAddrs :: V.Vector Fd -> FileOffset -> ByteCount -> [(Fd, FileOffset, ByteCount)]
 toPhyAddrs fds off0 len0 = go (quotRem' off0 defaultChunkSize) (fromIntegral len0)
   where
     go :: (ChunkIndex, FileOffset) -> ByteCount -> [(Fd, FileOffset, ByteCount)]
@@ -209,8 +199,8 @@ toPhyAddrs fds off0 len0 = go (quotRem' off0 defaultChunkSize) (fromIntegral len
       | otherwise = trans (chunkNo, off, defaultChunkSize - fromIntegral off) : go (chunkNo + 1, 0) (len - (defaultChunkSize - fromIntegral off))
     trans :: (ChunkIndex, FileOffset, ByteCount) -> (Fd, FileOffset, ByteCount)
     trans (chunkNo, off, bc) =
-      let (q, nodeNo) = chunkNo `quotRem` length (elems fds)
-      in (fds ! nodeNo, fromIntegral q * fromIntegral defaultChunkSize + off, bc)
+      let (q, nodeNo) = chunkNo `quotRem` V.length fds
+      in (fds V.! nodeNo, fromIntegral q * defaultChunkSize + off, bc)
 
 toPhyChunks :: B.ByteString -> [(Fd, FileOffset, ByteCount)] -> [(Fd, FileOffset, B.ByteString)]
 toPhyChunks buf ((fd, off, len):rest)
@@ -221,31 +211,49 @@ toPhyChunks _ _ = []
 
 hecsRead :: Config -> FilePath -> HT -> ByteCount -> FileOffset -> IO (Either Errno B.ByteString)
 hecsRead _ _ (HECS prims _) count off =
-  handle (\(_ :: SomeException) -> fmap Left getErrno) $
+  -- handle (\(_ :: SomeException) -> fmap Left getErrno) $
   fmap (Right . B.concat) (mapM readChunk . toPhyAddrs prims off . fromIntegral $ count )
-  where
-    readChunk :: (Fd, FileOffset, ByteCount) -> IO B.ByteString
-    readChunk (fd, goff, len) = do
-      _ <- fdSeek fd AbsoluteSeek goff
-      B.createAndTrim (fromIntegral len) (\ptr -> fmap fromIntegral (fdReadBuf fd ptr len))
 
 hecsWrite :: Config -> FilePath -> HT -> B.ByteString -> FileOffset -> IO (Either Errno ByteCount)
-hecsWrite _ _ (HECS prims _) src off =
-  handle (\(_ :: SomeException) -> fmap Left getErrno) $
-  fmap (Right . sum) (mapM writeChunk . toPhyChunks src . toPhyAddrs prims off . fromIntegral . B.length $ src)
-  where
-    writeChunk :: (Fd, FileOffset, B.ByteString) -> IO ByteCount
-    writeChunk (fd, goff, B.PS fptr soff len) = do
-      _ <- fdSeek fd AbsoluteSeek goff
-      withForeignPtr fptr $ \ptr -> fdWriteBuf fd (ptr `plusPtr` soff) (fromIntegral len)
+hecsWrite _ _ hecs@(HECS prims _) src off =
+  -- handle (\(_ :: SomeException) -> fmap Left getErrno) $
+  do
+    n0 <- fmap sum (mapM writeChunk . toPhyChunks src . toPhyAddrs prims off . fromIntegral . B.length $ src)
+    mapM_ (`completeStripe` hecs) [start..(end n0)]
+    return . Right $ n0
+      where start = fromIntegral off `quot` (V.length prims * defaultChunkSize)
+            end n = (fromIntegral off + fromIntegral n - 1) `quot` (V.length prims * defaultChunkSize)
+
+readChunk :: (Fd, FileOffset, ByteCount) -> IO B.ByteString
+readChunk (fd, goff, len) = do
+  _ <- fdSeek fd AbsoluteSeek goff
+  B.createAndTrim (fromIntegral len) (\ptr -> fmap fromIntegral (fdReadBuf fd ptr len))
+
+writeChunk :: (Fd, FileOffset, B.ByteString) -> IO ByteCount
+writeChunk (fd, goff, B.PS fptr soff len) = do
+  _ <- fdSeek fd AbsoluteSeek goff
+  withForeignPtr fptr $ \ptr -> fdWriteBuf fd (ptr `plusPtr` soff) (fromIntegral len)
+
+readStripe :: StripeIndex -> V.Vector Fd -> IO (V.Vector B.ByteString)
+readStripe si =
+  V.mapM (\fd -> readChunk (fd, fromIntegral si * defaultChunkSize, defaultChunkSize))
+
+writeStripe :: StripeIndex -> V.Vector Fd -> V.Vector B.ByteString -> IO (V.Vector ByteCount)
+writeStripe si =
+  V.zipWithM (\fd buf -> writeChunk (fd, fromIntegral si * defaultChunkSize, buf))
+
+completeStripe :: StripeIndex -> HECS -> IO ()
+completeStripe si (HECS prims secos) = do
+  _ <- readStripe si prims >>= writeStripe si secos
+  return ()
 
 hecsFlush :: Config -> FilePath -> HT -> IO Errno
 hecsFlush _ _ _ = return eOK
 
 hecsRelease :: Config -> FilePath -> HT -> IO ()
 hecsRelease _ _ (HECS prims secos) = do
-  mapM_ closeFd . elems $ prims
-  mapM_ closeFd . elems $ secos
+  V.mapM_ closeFd prims
+  V.mapM_ closeFd secos
 
 hecsSynchronizeFile :: Config -> FilePath -> SyncType -> IO Errno
 hecsSynchronizeFile _ _ _ = return eOK
@@ -269,6 +277,19 @@ hecsSetOwnerAndGroup cfg path0 uid gid =
        mapM_ (\path -> setOwnerAndGroup path uid gid) paths
        return eOK
 
+splitSize :: Int -> FileOffset -> [FileOffset]
+splitSize k sz = let (q, r) = sz `quotRem` (fromIntegral k * defaultChunkSize)
+                     go n (x:xs) | n > defaultChunkSize = (x + defaultChunkSize) : go (n - defaultChunkSize) xs
+                                 | otherwise = (x + n) : xs
+                     go _ [] = []
+                 in go r $ replicate k (q * defaultChunkSize)
+
+hecsSetFileSize :: Config -> FilePath -> FileOffset -> IO Errno
+hecsSetFileSize cfg path0 off =
+    do let paths = asPrimary path0 cfg
+       zipWithM_ setFileSize paths (splitSize (length paths) off)
+       return eOK
+
 -- hecsCreateSymbolicLink :: Config -> FilePath -> FilePath -> IO Errno
 -- hecsCreateSymbolicLink cfg src dest =
 --     do createSymbolicLink src dest
@@ -279,14 +300,5 @@ hecsSetOwnerAndGroup cfg path0 uid gid =
 --     do target <- readSymbolicLink . head $ asPrimary path cfg
 --        return (Right target)
 
-splitSize :: ByteCount -> ByteCount -> [ByteCount]
-splitSize k sz = let (q, r) = sz `quotRem` (fromIntegral k * defaultChunkSize)
-                     go n (x:xs) | n > defaultChunkSize = (x + defaultChunkSize) : go (n - defaultChunkSize) xs
-                                 | otherwise = (x + n) : xs
-                     go _ [] = []
-                 in go r $ replicate (fromIntegral k) (q * defaultChunkSize)
-
--- hecsSetFileSize :: Config -> FilePath -> FileOffset -> IO Errno
--- hecsSetFileSize cfg path off =
---     do setFileSize path off
---        return eOK
+-- hecsGetFileSystemStats :: String -> IO (Either Errno FileSystemStats)
+-- hecsGetFileSystemStats _ = return (Left eOK)
